935b999b704182c16d1214617f9bb76603472fb2
Yann Braga <yannbf@gmail.com>
2020-09-17 17:17:07 +0200

    chore(storybook): stories cleanup

--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { Story, Meta } from '@storybook/react/types-6-0';
+import { Story, Meta } from '@storybook/react';
 
 import { Button, ButtonProps } from './Button';
 
 export default {
-  title: 'Example/Button',
+  title: 'Button',
   component: Button,
   argTypes: {
     backgroundColor: { control: 'color' },
@@ -17,7 +16,7 @@ const Template: Story<ButtonProps> = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
   primary: true,
-  label: 'Buttony',
+  label: 'Button',
 };
 
 export const Secondary = Template.bind({});
