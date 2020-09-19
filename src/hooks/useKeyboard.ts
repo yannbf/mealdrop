cee5d1390beadd0ef61d32f83757b1b1756c4636
Yann Braga <yannbf@gmail.com>
2020-09-19 13:33:43 +0200

    feat(hooks): add useKey hook

---
+++ src/hooks/useKeyboard.ts
@@ -0,0 +1,45 @@
+import { useCallback, useEffect, useState } from 'react'
+
+export const useKey = (
+  key: string,
+  keyDownCb = () => {},
+  keyUpCb = () => {}
+): boolean => {
+  const [pressed, setPressed] = useState(false)
+  const match = useCallback(
+    (event: any) => key.toLowerCase() === event.key.toLowerCase(),
+    [key]
+  )
+
+  const onKeyDown = useCallback(
+    (event: KeyboardEvent) => {
+      if (match(event)) {
+        setPressed(true)
+        keyDownCb()
+      }
+    },
+    [keyDownCb, match]
+  )
+
+  const onKeyUp = useCallback(
+    (event: KeyboardEvent) => {
+      if (match(event)) {
+        setPressed(false)
+        keyUpCb()
+      }
+    },
+    [keyUpCb, match]
+  )
+
+  useEffect(() => {
+    window.addEventListener('keydown', onKeyDown)
+    window.addEventListener('keyup', onKeyUp)
+
+    return () => {
+      window.removeEventListener('keydown', onKeyDown)
+      window.removeEventListener('keyup', onKeyUp)
+    }
+  }, [key, onKeyDown, onKeyUp])
+
+  return pressed
+}
