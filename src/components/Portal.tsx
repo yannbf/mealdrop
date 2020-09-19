dc0cb0766c5081955dc442175e7f57e96de238ad
Yann Braga <yannbf@gmail.com>
2020-09-19 13:33:58 +0200

    feat: add portal component to project content

---
+++ src/components/Portal.tsx
@@ -0,0 +1,19 @@
+import React, { useRef, useEffect } from 'react'
+import { createPortal } from 'react-dom'
+
+type Props = {
+  selector: string
+}
+
+export const Portal: React.FC<Props> = ({ children, selector }) => {
+  const ref = useRef<Element>()
+
+  useEffect(() => {
+    const element = document.querySelector<Element>(selector)
+    if (!!element) {
+      ref.current = element
+    }
+  }, [selector])
+
+  return ref.current ? createPortal(children, ref.current) : null
+}
