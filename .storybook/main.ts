4f90fcdd490eacb7e05df89724cc1d72eb11f294
Yann Braga <yannbf@gmail.com>
2020-09-19 16:37:12 +0200

    chore(storybook): improve loading performance

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,8 +1,11 @@
 module.exports = {
-  stories: ['../**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/preset-create-react-app',
   ],
+  typescript: {
+    reactDocgen: 'react-docgen',
+  },
 }
