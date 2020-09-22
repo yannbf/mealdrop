c79b5b37aa8c1f3e93ad35907e51ef72cf4ee60c
Yann Braga <yannbf@gmail.com>
2020-09-22 19:59:11 +0200

    chore(storybook): configure viewports

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,29 @@
 import { withGlobalStyles, withRouter } from './decorators'
 import { withDesign } from 'storybook-addon-designs'
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
+import { viewports as breakpoints } from '../src/styles/breakpoints'
+
+// Create viewports using widths defined in design tokens
+const breakpointViewports = Object.keys(breakpoints).map((name) => {
+  return {
+    name: `Breakpoint - ${name}`,
+    styles: {
+      width: `${breakpoints[name]}px`,
+      // Account for padding and border around viewport preview
+      height: 'calc(100% - 20px)',
+    },
+    type: 'other',
+  }
+})
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  viewport: {
+    viewports: {
+      ...breakpointViewports,
+      ...INITIAL_VIEWPORTS,
+    },
+  },
 }
 
 export const decorators = [withGlobalStyles, withRouter, withDesign]
--- src/styles/breakpoints.ts
+++ src/styles/breakpoints.ts
@@ -1,15 +1,29 @@
 export const viewports = {
-  xSmall: 400,
-  small: 640,
-  medium: 768,
-  large: 1024,
-  xLarge: 1440,
+  XS: 400,
+  S: 640,
+  M: 768,
+  L: 1024,
+  XL: 1440,
 }
 
 export const breakpoints = {
-  XS: `(min-width: ${viewports.xSmall}px)`,
-  S: `(min-width: ${viewports.small}px)`,
-  M: `(min-width: ${viewports.medium}px)`,
-  L: `(min-width: ${viewports.large}px)`,
-  XL: `(min-width: ${viewports.xLarge}px)`,
+  XS: `(min-width: ${viewports.XS}px)`,
+  S: `(min-width: ${viewports.S}px)`,
+  M: `(min-width: ${viewports.M}px)`,
+  L: `(min-width: ${viewports.L}px)`,
+  XL: `(min-width: ${viewports.XL}px)`,
 }
+
+// const fallbackImage =
+//   'https://stylizedbay.com/wp-content/uploads/2018/02/unknown-avatar.jpg'
+// const apiKey = 'AIzaSyCzb6SI_JRrp6xLLYV617Ary6n59h36ros'
+// const cx = '004286675445984025592:ypgpkv9fjd4'
+// const baseUrl = `https://www.googleapis.com/customsearch/v1`
+
+// const getCharacterImage = async (name) => {
+//   const url = `${baseUrl}?key=${apiKey}&cx=${cx}&searchType=image&q=image::ghibli%20${name}`
+//   const request = await fetch(url)
+//   const result = await request.json()
+//   const characterImage = result?.items[0]?.link
+//   return characterImage || fallbackImage
+// }
