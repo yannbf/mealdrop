44e945747cd5b538b391f06153cfe1582795ef79
Yann Braga <yannbf@gmail.com>
2020-10-07 08:51:35 +0200

    fix(storybook): pass content as children to button

--- src/components/modal/Modal.stories.tsx
+++ src/components/modal/Modal.stories.tsx
@@ -21,10 +21,17 @@ const Template: Story = () => {
   const [isOpen, setIsOpen] = useState(false)
   const openModal = () => setIsOpen(true)
   const closeModal = () => setIsOpen(false)
+
+  React.useEffect(() => {
+    setIsOpen(true)
+  }, [])
+
   return (
     <>
       <p>Press ESC to close modal or click on the close icon!</p>
-      <Button primary label="Open modal" onClick={openModal} />
+      <Button primary onClick={openModal}>
+        Open modal
+      </Button>
       <Modal
         isOpen={isOpen}
         onClose={() => {
