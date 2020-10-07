1f4b60852d682b28f6cb8f942f02ea031f5cd073
Yann Braga <yannbf@gmail.com>
2020-10-07 09:27:09 +0200

    style(food-item): improve visuals

--- src/components/FoodItem.tsx
+++ src/components/FoodItem.tsx
@@ -1,36 +1,48 @@
 import React, { memo } from 'react'
 import styled, { css } from 'styled-components'
+import { toEuro } from '../helpers'
+import { Body } from './typography/Body'
+import { Heading } from './typography/Heading'
 
 const Container = styled.div<{ isHighlighted: boolean }>(
   ({ isHighlighted }) => css`
-    padding: 1rem;
-    border: 1px solid;
-    border-width: ${isHighlighted ? '2px' : '1px'};
-    border-color: ${isHighlighted ? '#a2b' : '#ccc'};
-    transition: border 0.1s linear;
-    transition: all 150ms ease-in;
+    padding: 1.5rem;
+    border-radius: 4px;
+    transition: box-shadow 0.1s ease-in;
     position: relative;
+    background: white;
 
     &:hover {
-      border-color: ${isHighlighted ? '#a2b' : '#333'};
       cursor: pointer;
-      box-shadow: 0px 18px 20px 0px rgba(0, 0, 0, 0.08);
+      box-shadow: 0px 14px 26px 0px rgba(0, 0, 0, 0.08);
     }
   `
 )
 
-const Quantity = styled.div`
+const Quantity = styled(Body)`
   padding: 0.25rem;
   width: 35px;
   height: 35px;
+  border-radius: 4px;
   display: flex;
   align-items: center;
   justify-content: center;
-  background: #dbbeff;
-  color: white;
+  background: #F8FDEE;
+  color: #77835E;
   position: absolute;
   top: 0;
   right: 0;
+`
+
+const Description = styled(Body)`
+  margin: 0;
+  color: #636871;
+  margin-top: 0.5rem;
+`
+
+const Price = styled(Body)`
+  margin: 0;
+  margin-top: 1rem;
 `
 
 export const FoodItem = memo(({ item, onClick }: any) => {
@@ -39,11 +51,12 @@ export const FoodItem = memo(({ item, onClick }: any) => {
   return (
     <Container isHighlighted={quantity > 0} onClick={onClick}>
       <div>
-        {quantity > 0 && <Quantity>{quantity}</Quantity>}
-        <h3>
-          {name} - {price}
-        </h3>
-        <p>{description}</p>
+        {quantity > 0 && <Quantity type="span" fontWeight="medium">{quantity}</Quantity>}
+        <Heading level={4}>
+          {name}
+        </Heading>
+        <Description>{description}</Description>
+        <Price>{toEuro(price)}</Price>
       </div>
     </Container>
   )
