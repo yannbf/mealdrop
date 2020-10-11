a67b3872c9bf544ea59d88d9905fbfe5ed941454
Yann Braga <yannbf@gmail.com>
2020-10-11 20:16:51 +0200

    chore(storybook): add theme switcher

--- .storybook/decorators.tsx
+++ .storybook/decorators.tsx
@@ -1,14 +1,9 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { ThemeProvider } from 'styled-components'
+import styled, { css, ThemeProvider } from 'styled-components'
+import { breakpoints } from '../src/styles/breakpoints'
 import { GlobalStyle } from '../src/styles/GlobalStyle'
-import { defaultTheme } from '../src/styles/theme'
-
-export const withTheme = (StoryFn) => (
-  <ThemeProvider theme={defaultTheme}>
-    <StoryFn />
-  </ThemeProvider>
-)
+import { darkTheme, lightTheme } from '../src/styles/theme'
 
 export const withRouter = (StoryFn) => (
   <Router>
@@ -16,11 +11,63 @@ export const withRouter = (StoryFn) => (
   </Router>
 )
 
-export const withGlobalStyles = (StoryFn) => (
-  <>
-    <GlobalStyle />
-    <StoryFn />
-  </>
+const ThemeBlock = styled.div<{ left?: boolean; fullScreen?: boolean }>(
+  ({ left, fullScreen, theme: { color } }) =>
+    css`
+      position: absolute;
+      top: 0;
+      left: ${left ? 0 : '50vw'};
+      border-right: ${left ? '1px solid #202020' : 'none'};
+      right: ${left ? '50vw' : 0};
+      width: 50vw;
+      height: 100vh;
+      bottom: 0;
+      overflow: auto;
+      padding: ${fullScreen ? 0 : '1rem'};
+      background: ${color.screenBackground};
+      ${breakpoints.S} {
+        left: ${left ? 0 : '50vw'};
+        right: ${left ? '50vw' : 0};
+        padding: 0 !important;
+      }
+    `
 )
 
-export const appDecorators = [withTheme, withRouter, withGlobalStyles]
+export const withTheme = (
+  StoryFn,
+  { globals: { theme = 'light' }, parameters }
+) => {
+  const fullScreen = parameters.layout === 'fullscreen'
+  const appTheme = theme === 'light' ? lightTheme : darkTheme
+
+  switch (theme) {
+    case 'side-by-side': {
+      return (
+        <>
+          <ThemeProvider theme={lightTheme}>
+            <GlobalStyle />
+            <ThemeBlock left fullScreen={fullScreen}>
+              <StoryFn />
+            </ThemeBlock>
+          </ThemeProvider>
+          <ThemeProvider theme={darkTheme}>
+            <GlobalStyle />
+            <ThemeBlock fullScreen={fullScreen}>
+              <StoryFn />
+            </ThemeBlock>
+          </ThemeProvider>
+        </>
+      )
+    }
+    default: {
+      return (
+        <ThemeProvider theme={appTheme}>
+          <GlobalStyle />
+          <StoryFn />
+        </ThemeProvider>
+      )
+    }
+  }
+}
+
+export const appDecorators = [withTheme, withRouter]
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -26,4 +26,20 @@ export const parameters = {
   },
 }
 
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Theme for the components',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'circlehollow',
+      items: [
+        { value: 'light', icon: 'circlehollow', title: 'light' },
+        { value: 'dark', icon: 'circle', title: 'dark' },
+        { value: 'side-by-side', icon: 'sidebar', title: 'side by side' },
+      ],
+    },
+  },
+}
+
 export const decorators = [...appDecorators, withDesign]
