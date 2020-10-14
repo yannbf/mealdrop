67f68c48416e3be850b97be66cd294a6843c0b06
Yann Braga <yannbf@gmail.com>
2020-10-14 23:11:10 +0200

    chore(stubs): update cart items

--- src/stub/cart-items.ts
+++ src/stub/cart-items.ts
@@ -1,14 +1,41 @@
 export const cartItems = [
   {
-    id: 1,
-    name: 'Cheeseburger',
-    price: 5.5,
-    quantity: 2,
+    id: 2,
+    name: 'Fries',
+    description: 'Fried french fries',
+    price: 2.5,
+    quantity: 1,
   },
   {
     id: 1,
-    name: 'Cola',
-    price: 2.5,
-    quantity: 3,
+    name: 'Cheeseburger',
+    description: 'Nice grilled burger with cheese',
+    price: 8.5,
+    quantity: 1,
+  },
+  {
+    id: 3,
+    name: 'Vanilla ice cream',
+    description: 'Ice cream',
+    price: 2,
+    quantity: 1,
+  },
+  {
+    id: 4,
+    name: 'Coca-Cola',
+    price: 1.75,
+    quantity: 1,
+  },
+  {
+    id: 5,
+    name: 'Fanta',
+    price: 1.5,
+    quantity: 1,
+  },
+  {
+    id: 6,
+    name: 'Sprite',
+    price: 1.5,
+    quantity: 1,
   },
 ]
