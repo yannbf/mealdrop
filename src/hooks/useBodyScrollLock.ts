e2a11b73d227a6a4201388ec5442a470b78cbe17
Yann Braga <yannbf@gmail.com>
2020-10-14 23:13:24 +0200

    fix: add mobile scroll lock

--- src/hooks/useBodyScrollLock.ts
+++ src/hooks/useBodyScrollLock.ts
@@ -3,14 +3,14 @@ import { useLayoutEffect } from 'react'
 export const useLockBodyScroll = (shouldLock = false) => {
   useLayoutEffect(() => {
     document.body.style.overflow = shouldLock ? 'hidden' : 'auto'
-    if (!/Mobi/i.test(window.navigator.userAgent)) {
-      document.body.style.paddingRight = shouldLock ? '15px' : '0'
+    if (/Mobi/i.test(window.navigator.userAgent)) {
+      document.body.style.height = shouldLock ? '100vh' : 'auto'
     }
     // Re-enable scrolling when component unmounts
     return () => {
       document.body.style.overflow = 'auto'
-      if (!/Mobi/i.test(window.navigator.userAgent)) {
-        document.body.style.paddingRight = '15px'
+      if (/Mobi/i.test(window.navigator.userAgent)) {
+        document.body.style.height = 'auto'
       }
     }
   }, [shouldLock])
