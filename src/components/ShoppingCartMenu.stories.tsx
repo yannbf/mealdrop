2176475025f7ef0f2c849ef4c4a2ba28f602559a
Yann Braga <yannbf@gmail.com>
2020-10-14 23:15:02 +0200

    chore(storybook): add shopping cart menu stories

---
+++ src/components/ShoppingCartMenu.stories.tsx
@@ -0,0 +1,46 @@
+import { Story } from '@storybook/react'
+import React, { useState } from 'react'
+import { cartItems } from '../stub/cart-items'
+import { Button } from './Button'
+
+import { ShoppingCartMenu, ShoppingCartMenuProps } from './ShoppingCartMenu'
+
+export default {
+  title: 'Features/ShoppingCartMenu',
+  component: ShoppingCartMenu,
+}
+
+const Template: Story<ShoppingCartMenuProps> = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const openShoppingCartMenu = () => setIsOpen(true)
+  const closeShoppingCartMenu = () => setIsOpen(false)
+
+  React.useEffect(() => {
+    setIsOpen(true)
+  }, [])
+
+  return (
+    <>
+      <p>Press ESC to close the ShoppingCartMenu or click on the close icon!</p>
+      <Button onClick={openShoppingCartMenu}>Open ShoppingCartMenu</Button>
+      <ShoppingCartMenu
+        isOpen={isOpen}
+        cartItems={cartItems}
+        totalPrice={1200}
+        onItemChange={() => {}}
+        onClose={() => {
+          closeShoppingCartMenu()
+        }}
+      />
+    </>
+  )
+}
+
+export const Desktop = Template.bind({})
+
+export const Mobile = Template.bind({})
+Mobile.parameters = {
+  viewport: {
+    defaultViewport: 'iphonex',
+  },
+}
