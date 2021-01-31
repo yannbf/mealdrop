7912d5718f8d5aa1779b659c5a1b75550d6dc468
Yann Braga <yannbf@gmail.com>
2021-01-31 18:27:19 +0100

    chore(multistep-form): small improvements

--- src/components/registration-form/MultiStepForm.stories.tsx
+++ src/components/registration-form/MultiStepForm.stories.tsx
@@ -1,12 +1,12 @@
-import { Story } from '@storybook/react'
 import React from 'react'
+import { Story, Meta } from '@storybook/react'
 
 import { MultiStepForm } from './MultiStepForm'
 
 export default {
   title: 'Features/MultiStepForm',
   component: MultiStepForm,
-}
+} as Meta
 
 const Template: Story = (args) => <MultiStepForm {...args} />
 export const Default = Template.bind({})
