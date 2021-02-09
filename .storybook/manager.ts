1e1656fae0a54d536f8c82832160120de655bb8a
Yann Braga <yannbf@gmail.com>
2021-02-10 00:28:31 +0100

    chore(storybook): add manager with custom theme and options

---
+++ .storybook/manager.ts
@@ -0,0 +1,47 @@
+import { addons } from '@storybook/addons'
+import { create } from '@storybook/theming/create'
+import logo from '../src/assets/images/logo-black.svg'
+
+const theme = create({
+  base: 'light',
+
+  // Base colors
+  colorSecondary: '#9FC7CD', // blue-dark-1
+
+  // UI
+  appBg: '#F6F9FC',
+  appContentBg: '#FFFFFF',
+  appBorderColor: 'rgba(0,0,0,.1)',
+  appBorderRadius: 4,
+
+  // Typography
+  fontBase: '"Open Sans", sans-serif',
+  fontCode: 'monospace',
+
+  // Text colors
+  textColor: '#333333',
+  textInverseColor: '#FFFFFF',
+  textMutedColor: '#666666',
+
+  // Toolbar default and active colors
+  barTextColor: '#999999',
+  barSelectedColor: '#9FC7CD',
+  barBg: '#FFFFFF',
+
+  // Form colors
+  inputBg: '#FFFFFF',
+  inputBorder: 'rgba(0,0,0,.3)',
+  inputTextColor: '#333333',
+  inputBorderRadius: 4,
+
+  // Brand assets
+  brandTitle: 'Mealdrop',
+  brandUrl: 'https://mealdrop.netlify.com/',
+  brandImage: logo,
+})
+
+addons.setConfig({
+  theme,
+  showPanel: true, // show addons panel by default
+  panelPosition: 'right', // position addons panel on the right by default
+})
