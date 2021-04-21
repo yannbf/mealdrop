3e21b114c68c60b5f087eccea425f1b88cd1de5e
Yann Braga <yannbf@gmail.com>
2021-04-22 00:33:54 +0200

    chore: add design link to badge

--- src/components/Badge/Badge.stories.tsx
+++ src/components/Badge/Badge.stories.tsx
@@ -6,6 +6,14 @@ import { Badge, BadgeProps } from './Badge'
 export default {
   title: 'Components/Badge',
   component: Badge,
+  parameters: {
+    design: {
+      type: 'experimental-figspec',
+      url:
+        'https://www.figma.com/file/XW4Bcjmj3JOILjKmZjjdQd/Mealdrop?node-id=780%3A2938',
+      accessToken: process.env.FIGMA_ACCESS_TOKEN,
+    },
+  },
 } as Meta
 
 const Basic: Story<BadgeProps> = (args) => <Badge {...args} />
