d82d74480fb7c4ffcb7d7a3292a8fc000089a960
Yann Braga <yannbf@gmail.com>
2021-05-04 21:56:25 +0200

    fix: add typings to breakpointViewports

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -13,14 +13,14 @@ const breakpointViewports = Object.keys(breakpoints).reduce((acc, key) => {
   acc[`breakpoint${key}`] = {
     name: `Breakpoint - ${key}`,
     styles: {
-      width: `${breakpoints[key]}px`,
+      width: `${breakpoints[key as keyof typeof breakpoints]}px`,
       // Account for padding and border around viewport preview
       height: 'calc(100% - 20px)',
     },
     type: 'other',
   }
   return acc
-}, {})
+}, {} as typeof INITIAL_VIEWPORTS)
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
