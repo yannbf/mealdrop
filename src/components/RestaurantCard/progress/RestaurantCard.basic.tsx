890dd3dd72ef52bf730271c386fe5d7fc84c833f
Yann Braga <yannbf@gmail.com>
2021-06-20 00:56:20 +0200

    chore: add evolution versions of restaurantcard

---
+++ src/components/RestaurantCard/progress/RestaurantCard.basic.tsx
@@ -0,0 +1,31 @@
+
+type RestaurantCardProps = {
+  id?: string
+  name: string
+  rating?: number
+  specialty: string
+  photoUrl: string
+  isClosed?: boolean
+  categories?: string[]
+  isLoading?: boolean
+  isNew?: boolean
+  className?: string
+}
+
+export const RestaurantCard = ({
+  isClosed = false,
+  isNew = false,
+  isLoading = false,
+}: RestaurantCardProps) => {
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <div>
+      {isNew && <div>new</div>}
+      {isClosed && <div>closed</div>}
+      Restaurant card
+    </div>
+  )
+}
---
+++ src/components/RestaurantCard/progress/RestaurantCard.unstyled.tsx
@@ -0,0 +1,58 @@
+import { Badge } from "../../Badge"
+import { Review } from "../../Review"
+import { Body, Heading } from "../../typography"
+
+type RestaurantCardProps = {
+  id?: string
+  name: string
+  rating?: number
+  specialty: string
+  photoUrl: string
+  isClosed?: boolean
+  categories?: string[]
+  isLoading?: boolean
+  isNew?: boolean
+  onClick: () => void
+  className?: string
+}
+
+export const RestaurantCard = ({
+  photoUrl,
+  name,
+  specialty,
+  rating,
+  isClosed = false,
+  isLoading = false,
+  categories,
+  isNew = false,
+  className,
+  onClick
+}: RestaurantCardProps) => {
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <div>
+      {isNew && <div>new</div>}
+      <div>
+        {isClosed && (
+          <Body type="span">This restaurant is closed.</Body>
+        )}
+        <img
+          src={photoUrl}
+          alt="restaurant"
+        />
+      </div>
+      <div>
+        <Heading level={2}>{name} </Heading>
+        <Review rating={rating} />
+        <Body fontWeight="regular">{specialty}</Body>
+        {categories?.map((category) => (
+          <Badge key={category} text={category} />
+        ))}
+      </div>
+    </div>
+  )
+}
