b6cad00e211cd7bfe68b3c9470e8ddff6ccdc997
Yann Braga <yannbf@gmail.com>
2021-08-17 23:47:50 +0200

    chore: configure lint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb-typescript', 'prettier'],
+  extends: ['plugin:react/recommended', 'airbnb-typescript', 'plugin:prettier/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -11,16 +11,33 @@ module.exports = {
     },
     ecmaVersion: 12,
     sourceType: 'module',
-    project: 'tsconfig.eslint.json'
+    project: 'tsconfig.eslint.json',
   },
   plugins: ['react', '@typescript-eslint'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   rules: {
-    'import/order': ["error", {"newlines-between": "always"}],
+    'prettier/prettier': 'warn',
+    'import/order': ['error', { 'newlines-between': 'always' }],
+    '@typescript-eslint/no-use-before-define': 'off',
     'react/jsx-uses-react': 'off',
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
     'import/prefer-default-export': 'off',
     'import/extensions': 'off',
-    'react/require-default-props': 'off'
+    'react/require-default-props': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'no-nested-ternary': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-console': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react/no-unused-prop-types': 'off',
+    'no-param-reassign': 'off',
+    'class-methods-use-this': 'off',
+    'prefer-promise-reject-errors': 'off',
+    'react/prefer-stateless-function': 'off',
   },
 }
--- .prettierrc.js
+++ .prettierrc.js
@@ -3,4 +3,6 @@ module.exports = {
   tabWidth: 2,
   semi: false,
   singleQuote: true,
+  printWidth: 120,
+  bracketSpacing: true,
 }
