2a506171fd1ea23334a8b85e4c72d982d8d7112b
Yann Braga <yannbf@gmail.com>
2021-09-12 17:42:41 +0200

    feat: add conditional logic api

---
+++ src/api/conditional-logic.ts
@@ -0,0 +1,79 @@
+/* eslint-disable max-classes-per-file */
+import axios, { AxiosResponse } from 'axios'
+
+import { Restaurant } from '../types'
+import { restaurants as restaurantsMock } from '../stub/restaurants'
+
+interface BaseApi {
+  getCuratedRestaurants: () => Promise<Restaurant[]>
+  getRestaurantById: (id: string) => Promise<Restaurant>
+  getRestaurantsByCategory: (category: string) => Promise<Restaurant[]>
+}
+
+const BASE_URL = 'https://blab-290ab.firebaseio.com'
+
+const isMockedEnvironment = !!process.env.STORYBOOK || process.env.NODE_ENV === 'test'
+
+const apiCache = new Map()
+
+const apiGet = async <T>(url: string): Promise<AxiosResponse<T>> => {
+  // do not cache when testing to avoid flakyness
+  if (!isMockedEnvironment && apiCache.has(url)) {
+    return apiCache.get(url)
+  }
+
+  const response = await axios.get<T>(url)
+
+  if (!isMockedEnvironment) {
+    apiCache.set(url, response)
+  }
+
+  return response
+}
+
+class RestaurantsApi implements BaseApi {
+  async getCuratedRestaurants() {
+    const { data: restaurants } = await apiGet<Restaurant[]>(`${BASE_URL}/restaurants/.json`)
+
+    return restaurants
+  }
+
+  async getRestaurantById(id: string) {
+    const { data: restaurant } = await apiGet<Restaurant>(`${BASE_URL}/restaurants/${id}/.json`)
+
+    // firebase returns 200 with null when not found, so we need to force the correct status
+    if (restaurant == null) {
+      return Promise.reject({ response: { status: 404 } })
+    }
+
+    return restaurant
+  }
+
+  async getRestaurantsByCategory(category: string) {
+    const restaurants = await this.getCuratedRestaurants()
+
+    return restaurants
+      .filter((restaurant) => restaurant.categories?.includes(category.toLowerCase()))
+      .sort((restaurant) => (restaurant.isClosed ? 1 : -1))
+      .sort((restaurant) => (restaurant.isNew ? -1 : 1))
+  }
+}
+
+class MockedRestaurantsApi implements BaseApi {
+  async getCuratedRestaurants() {
+    return restaurantsMock
+  }
+
+  async getRestaurantById(id: string) {
+    return restaurantsMock.find((restaurant) => restaurant.id === id) as Restaurant
+  }
+
+  async getRestaurantsByCategory(category: string) {
+    return restaurantsMock
+      .filter((restaurant) => restaurant.categories?.includes(category.toLowerCase()))
+      .sort((restaurant) => (restaurant.isClosed ? 1 : -1))
+      .sort((restaurant) => (restaurant.isNew ? -1 : 1))
+  }
+}
+
+export const api: BaseApi = isMockedEnvironment ? new MockedRestaurantsApi() : new RestaurantsApi()
