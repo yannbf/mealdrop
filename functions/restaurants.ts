d9dc65f8d98d580bc1c9033b8e89cccceac8cc92
Yann Braga <yannbf@gmail.com>
2021-09-12 18:43:26 +0200

    feat: add cors headers to lambda function

--- functions/restaurants.ts
+++ functions/restaurants.ts
@@ -11,13 +11,29 @@ const getRestaurantsByCategory = (category: string) => {
     .sort((restaurant) => (restaurant.isNew ? -1 : 1))
 }
 
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Methods': 'GET, POST, OPTION',
+}
+
 export const handler = async (event: any) => {
   const { id, category } = event.queryStringParameters
 
   if (id) {
+    const restaurant = getRestaurantById(id)
+
+    if (!restaurant) {
+      return {
+        statusCode: 404,
+        headers,
+      }
+    }
+
     return {
       statusCode: 200,
       body: JSON.stringify(getRestaurantById(id)),
+      headers,
     }
   }
 
@@ -25,11 +41,13 @@ export const handler = async (event: any) => {
     return {
       statusCode: 200,
       body: JSON.stringify(getRestaurantsByCategory(category)),
+      headers,
     }
   }
 
   return {
     statusCode: 200,
     body: JSON.stringify(restaurants),
+    headers,
   }
 }
