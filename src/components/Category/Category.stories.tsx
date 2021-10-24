6452a9b24866bad80c62de9b6f78343e008b90c7
Yann Braga <yannbf@gmail.com>
2021-10-24 23:33:40 +0200

    style: update category component

--- src/components/Category/Category.stories.tsx
+++ src/components/Category/Category.stories.tsx
@@ -5,19 +5,18 @@ import { Category } from './Category'
 export default {
   title: 'Components/Category',
   component: Category,
+  args: {
+    title: 'Pizza',
+    photoUrl:
+      'https://images.pexels.com/photos/2147491/pexels-photo-2147491.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=550',
+  },
 } as ComponentMeta<typeof Category>
 
 const Template: ComponentStory<typeof Category> = (args) => <Category {...args} />
 
 export const Default = Template.bind({})
-Default.args = {
-  title: 'Pizza',
-  photoUrl:
-    'https://images.pexels.com/photos/2147491/pexels-photo-2147491.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=550',
-}
 
 export const Rounded = Template.bind({})
 Rounded.args = {
-  ...Default.args,
-  rounded: true,
+  round: true,
 }
