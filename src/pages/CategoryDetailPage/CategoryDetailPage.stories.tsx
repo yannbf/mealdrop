ef9d0234ca03ec4b9a76eb787d7d38e284130b16
Yann Braga <yannbf@gmail.com>
2021-11-15 00:04:33 +0100

    chore(storybook): add mocks for category detail page

--- src/pages/CategoryDetailPage/CategoryDetailPage.stories.tsx
+++ src/pages/CategoryDetailPage/CategoryDetailPage.stories.tsx
@@ -1,6 +1,9 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { rest } from 'msw'
 
+import { restaurants } from '../../stub/restaurants'
 import { DefaultTemplate } from '../../templates/PageTemplate'
+import { BASE_URL } from '../../api'
 
 import { CategoryDetailPage } from './CategoryDetailPage'
 
@@ -20,10 +23,12 @@ const Template: ComponentStory<typeof CategoryDetailPage> = () => (
 
 export const Default = Template.bind({})
 Default.parameters = {
-  deeplink: { route: '/categories/pizza', path: '/categories/:id' },
+  deeplink: { route: '/categories/burgers', path: '/categories/:id' },
+  msw: [rest.get(BASE_URL, (req, res, ctx) => res(ctx.json([restaurants[0]])))],
 }
 
 export const Missing = Template.bind({})
 Missing.parameters = {
   deeplink: { route: '/categories/wrong', path: '/categories/:id' },
+  msw: [rest.get(BASE_URL, (req, res, ctx) => res(ctx.json([])))],
 }
