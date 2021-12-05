1f5e590b8c73f2bd996b36e69e1a893d9dc13e35
Yann Braga <yannbf@gmail.com>
2021-12-05 21:40:31 +0100

    fix: improve logic in Review component

--- src/components/Review/Review.tsx
+++ src/components/Review/Review.tsx
@@ -19,12 +19,12 @@ const getReview = (rating?: number) => {
 
   let reviewText = 'Very poor'
 
-  if (rating === 5) {
-    reviewText = 'Excellent'
-  } else if (rating >= 4 && rating <= 5) {
-    reviewText = 'Very good'
-  } else if (rating >= 2 && rating <= 4) {
+  if (rating >= 2 && rating < 4) {
     reviewText = 'Adequate'
+  } else if (rating >= 4 && rating < 5) {
+    reviewText = 'Very good'
+  } else if (rating >= 5) {
+    reviewText = 'Excellent'
   }
 
   return `★ ${rating.toFixed(1)} ${reviewText}`
