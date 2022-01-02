3d12e6622c4851732125fa6c6024591e9fd50bb9
Yann Braga <yannbf@gmail.com>
2022-01-02 14:27:34 +0100

    test(restaurant-card): add a11y tests

--- src/components/RestaurantCard/RestaurantCard.test.tsx
+++ src/components/RestaurantCard/RestaurantCard.test.tsx
@@ -1,13 +1,17 @@
 import { render, screen } from '@testing-library/react'
 import { composeStories } from '@storybook/testing-react'
+import { axe, toHaveNoViolations } from 'jest-axe'
 
 import * as stories from './RestaurantCard.stories'
+
+expect.extend(toHaveNoViolations)
 
 const { Default, Loading, New, Closed } = composeStories(stories)
 
 describe('RestaurantCard', () => {
   test('should render correctly', () => {
     render(<Default />)
+
     expect(screen.getByText('Burger King')).toBeInTheDocument()
   })
 
@@ -32,4 +36,17 @@ describe('RestaurantCard', () => {
     screen.getByTestId('restaurant-card').click()
     expect(onClickSpy).not.toHaveBeenCalled()
   })
+})
+
+// Go through every story from composeStories and create a map of StoryName <-> StoryComponent
+const testCases = Object.values(composeStories(stories)).map((Story) => [
+  // The ! is necessary in Typescript only, as the property is part of a partial type
+  Story.storyName!,
+  Story,
+])
+
+// Go through all test cases to batch test accessibility
+test.each(testCases)('%s story should be accessible', async (_storyName, Story) => {
+  const { container } = render(<Story />)
+  expect(await axe(container)).toHaveNoViolations()
 })
