e6b0bdb7fe8c27b3637e7069dc91ad39b83393be
Yann Braga <yannbf@gmail.com>
2022-01-30 18:58:14 +0100

    refactor: improve app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import useDarkMode from 'use-dark-mode'
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider as StoreProvider } from 'react-redux'
 
+import { store } from './app-state'
 import { AppRoutes } from './Routes'
 import { lightTheme, darkTheme } from './styles/theme'
 import { GlobalStyle } from './styles/GlobalStyle'
@@ -10,9 +13,13 @@ export default function App() {
   const theme = value ? darkTheme : lightTheme
 
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <AppRoutes />
-    </ThemeProvider>
+    <Router>
+      <StoreProvider store={store}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <AppRoutes />
+        </ThemeProvider>
+      </StoreProvider>
+    </Router>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,12 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
-import { Provider as StoreProvider } from 'react-redux'
 
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-import { store } from './app-state'
 
 ReactDOM.render(
   <StrictMode>
-    <Router>
-      <StoreProvider store={store}>
-        <App />
-      </StoreProvider>
-    </Router>
+    <App />
   </StrictMode>,
   document.getElementById('root')
 )
