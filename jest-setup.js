42c37f0c1f463f897589f91ccaf8cfabbc090d6a
Yann Braga <yannbf@gmail.com>
2022-02-08 20:14:46 +0100

    update test runner

---
+++ jest-setup.js
@@ -0,0 +1,31 @@
+const { toMatchImageSnapshot } = require('jest-image-snapshot');
+const { setPreRender, setPostRender } = require('@storybook/test-runner');
+
+expect.extend({ toMatchImageSnapshot });
+
+// use custom directory/id to align CSF and stories.json mode outputs
+const customSnapshotsDir = `${process.cwd()}/__snapshots__`;
+
+setPreRender(async (page, context) => {
+  if(context.title === 'UserFlows/App') {
+    const image = await page.screenshot();
+    expect(image).toMatchImageSnapshot({
+      customSnapshotsDir,
+      customSnapshotIdentifier: context.id + '-before',
+      failureThreshold: 0.03,
+      failureThresholdType: 'percent',
+    });
+  }
+});
+
+setPostRender(async (page, context) => {
+  if(context.title === 'UserFlows/App') {
+    const image = await page.screenshot();
+    expect(image).toMatchImageSnapshot({
+      customSnapshotsDir,
+      customSnapshotIdentifier: context.id+ '-after',
+      failureThreshold: 0.03,
+      failureThresholdType: 'percent',
+    });
+  }
+});
