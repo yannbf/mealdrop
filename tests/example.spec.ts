264894eb34ed118db098886a9c8cd5b3aec2770c
Steven Kitterman <steven.kitterman@gmail.com>
2023-12-07 18:15:55 -0800

    Increase tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,8 +1,25 @@
-import { test, expect } from '@chromaui/test-archiver'
+import { test, expect, takeArchive } from '@chromaui/test-archiver'
 
-test('has title', async ({ page }) => {
+test('Home page loads', async ({ page }) => {
   await page.goto('http://localhost:3000')
 
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Mealdrop - find your next meal/)
+
+  await page.waitForTimeout(3000)
+})
+
+test('Order created', async ({ page }, testInfo) => {
+  await page.goto('http://localhost:3000/restaurants/2')
+
+  await page.waitForTimeout(1000)
+  await takeArchive(page, testInfo)
+
+  await page.getByText('Fries').first().click()
+
+  await expect(page.getByTestId('modal')).toBeVisible()
+  await takeArchive(page, testInfo)
+
+  await page.getByText(/add for €2\.50/).click()
+
+  await page.waitForTimeout(3000)
 })
