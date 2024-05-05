2ace4a26ab45d568adf7764b8cafa1124739ea11
Yann Braga <yannbf@gmail.com>
2024-05-05 20:00:24 +0200

    add missing skeleton style

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -3,6 +3,7 @@ import { createGlobalStyle, css } from 'styled-components'
 import { breakpoints } from './breakpoints'
 import { resetCSS } from './CSSReset'
 import 'react-multi-carousel/lib/styles.css'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 export const GlobalStyle = createGlobalStyle(
   ({ theme: { color } }) => css`
@@ -17,7 +18,9 @@ export const GlobalStyle = createGlobalStyle(
       font-style: normal;
       font-weight: 400;
       font-display: swap;
-      src: local('Montserrat Regular'), local('Montserrat-Regular'),
+      src:
+        local('Montserrat Regular'),
+        local('Montserrat-Regular'),
         url(https://fonts.gstatic.com/s/montserrat/v15/JTUSjIg1_i6t8kCHKm459WlhyyTh89Y.woff2)
           format('woff2');
       unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC,
@@ -29,7 +32,9 @@ export const GlobalStyle = createGlobalStyle(
       font-style: normal;
       font-weight: 500;
       font-display: swap;
-      src: local('Montserrat Medium'), local('Montserrat-Medium'),
+      src:
+        local('Montserrat Medium'),
+        local('Montserrat-Medium'),
         url(https://fonts.gstatic.com/s/montserrat/v15/JTURjIg1_i6t8kCHKm45_ZpC3gnD_vx3rCs.woff2)
           format('woff2');
       unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC,
@@ -41,7 +46,9 @@ export const GlobalStyle = createGlobalStyle(
       font-style: normal;
       font-weight: 700;
       font-display: swap;
-      src: local('Montserrat Bold'), local('Montserrat-Bold'),
+      src:
+        local('Montserrat Bold'),
+        local('Montserrat-Bold'),
         url(https://fonts.gstatic.com/s/montserrat/v15/JTURjIg1_i6t8kCHKm45_dJE3gnD_vx3rCs.woff2)
           format('woff2');
       unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC,
@@ -53,7 +60,9 @@ export const GlobalStyle = createGlobalStyle(
       font-family: 'Montserrat';
       font-style: normal;
       font-weight: 900;
-      src: local('Montserrat Black'), local('Montserrat-Black'),
+      src:
+        local('Montserrat Black'),
+        local('Montserrat-Black'),
         url(https://fonts.gstatic.com/s/montserrat/v15/JTURjIg1_i6t8kCHKm45_epG3gnD_vx3rCs.woff2)
           format('woff2');
       unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC,
@@ -65,7 +74,9 @@ export const GlobalStyle = createGlobalStyle(
       font-style: normal;
       font-weight: 400;
       font-display: swap;
-      src: local('Hind Regular'), local('Hind-Regular'),
+      src:
+        local('Hind Regular'),
+        local('Hind-Regular'),
         url(https://fonts.gstatic.com/s/hind/v11/5aU69_a8oxmIdGl4BDGwgDI.woff2) format('woff2');
       unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC,
         U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
@@ -76,7 +87,9 @@ export const GlobalStyle = createGlobalStyle(
       font-style: normal;
       font-weight: 500;
       font-display: swap;
-      src: local('Hind Medium'), local('Hind-Medium'),
+      src:
+        local('Hind Medium'),
+        local('Hind-Medium'),
         url(https://fonts.gstatic.com/s/hind/v11/5aU19_a8oxmIfJpbERySjRhc9V0.woff2) format('woff2');
       unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC,
         U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
