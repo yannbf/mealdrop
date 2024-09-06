8ff8894c235a40994b5e5a18142b7777bb6e4635
Yann Braga <yannbf@gmail.com>
2024-09-06 17:41:17 +0200

    set up vitest plugin

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -13,6 +13,7 @@ const config: StorybookConfig = {
     '@chromatic-com/storybook',
     '@storybook/addon-a11y',
     '@storybook/addon-designs',
+    '@storybook/experimental-addon-test'
   ],
   staticDirs: ['../public'],
   framework: '@storybook/react-vite',
---
+++ vitest.workspace.ts
@@ -0,0 +1,29 @@
+import { defineWorkspace } from 'vitest/config';
+import { storybookTest } from '@storybook/experimental-addon-test/vite-plugin';
+
+
+// More info at: https://storybook.js.org/docs/writing-tests/test-runner-with-vitest
+export default defineWorkspace([
+  'vite.config.ts',
+  {
+    extends: 'vite.config.ts',
+    plugins: [
+      // See options at: https://storybook.js.org/docs/writing-tests/test-runner-with-vitest#storybooktest
+      storybookTest({
+        storybookScript: 'yarn storybook --ci'
+      }),
+    ],
+    test: {
+      name: 'storybook',
+      browser: {
+        enabled: true,
+        headless: true,
+        name: 'chromium',
+        provider: 'playwright',
+      },
+      // Make sure to adjust this pattern to match your stories files.
+      include: ['**/*.stories.?(m)[jt]s?(x)'],
+      setupFiles: ['./.storybook/vitest.setup.ts'],
+    },
+  },
+]);
