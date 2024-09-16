cd3a4b5e8bfa340d12505dd396ba60582b261b48
Valentin Palkovic <valentin@chromatic.com>
2024-09-16 16:01:50 +0200

    chore

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,17 +2,17 @@ import { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
   stories: [
-    '../src/docs/Introduction.mdx',
-    '../src/docs/*.mdx',
-    '../src/**/*.mdx',
+    // '../src/docs/Introduction.mdx',
+    // '../src/docs/*.mdx',
+    // '../src/**/*.mdx',
     '../src/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
-    '@chromatic-com/storybook',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-a11y',
-    '@storybook/addon-designs',
+    // '@chromatic-com/storybook',
+    // '@storybook/addon-essentials',
+    // '@storybook/addon-interactions',
+    // '@storybook/addon-a11y',
+    // '@storybook/addon-designs',
     '@storybook/experimental-addon-coverage'
   ],
   typescript: {
