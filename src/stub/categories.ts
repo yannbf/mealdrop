ec3c7c6cbe9f1ab372ed4f202e568b545b7f9948
winkerVSbecks <varunvachhar@gmail.com>
2024-12-11 21:41:30 -0500

    change category data

--- src/stub/categories.ts
+++ src/stub/categories.ts
@@ -12,6 +12,16 @@ export const categories = [
       'https://images.pexels.com/photos/2233351/pexels-photo-2233351.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=550',
   },
   {
+    title: 'thai',
+    photoUrl:
+      'https://images.unsplash.com/photo-1562565652-a0d8f0c59eb4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=20',
+  },
+  {
+    title: 'indian',
+    photoUrl:
+      'https://images.unsplash.com/photo-1567337710282-00832b415979?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1488&q=20',
+  },
+  {
     id: 'desserts',
     title: 'Desserts',
     photoUrl:
@@ -23,26 +33,16 @@ export const categories = [
     photoUrl:
       'https://images.pexels.com/photos/1199960/pexels-photo-1199960.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=550',
   },
-  {
-    id: 'sushi',
-    title: 'Sushi',
-    photoUrl:
-      'https://images.pexels.com/photos/9210/food-japanese-food-photography-sushi.jpg?auto=compress&cs=tinysrgb&dpr=2&h=550',
-  },
-  {
-    id: 'asian',
-    title: 'Asian',
-    photoUrl:
-      'https://images.pexels.com/photos/1234535/pexels-photo-1234535.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=550',
-  },
   // {
-  //   title: 'thai',
+  //   id: 'sushi',
+  //   title: 'Sushi',
   //   photoUrl:
-  //     'https://images.unsplash.com/photo-1562565652-a0d8f0c59eb4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=20',
+  //     'https://images.pexels.com/photos/9210/food-japanese-food-photography-sushi.jpg?auto=compress&cs=tinysrgb&dpr=2&h=550',
   // },
   // {
-  //   title: 'indian',
+  //   id: 'asian',
+  //   title: 'Asian',
   //   photoUrl:
-  //     'https://images.unsplash.com/photo-1567337710282-00832b415979?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1488&q=20',
+  //     'https://images.pexels.com/photos/1234535/pexels-photo-1234535.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=550',
   // },
 ]
