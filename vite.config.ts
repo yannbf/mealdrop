25a392e7edc60dfbcce381bdc4e1c7f0d3002984
Yann Braga <yannbf@gmail.com>
2025-02-05 17:56:20 +0100

    update to Storybook 8.5

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import viteTsconfigPaths from 'vite-tsconfig-paths'
 import svgrPlugin from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  plugins: [react(),  svgrPlugin()],
   build: {
     outDir: 'build',
   },
