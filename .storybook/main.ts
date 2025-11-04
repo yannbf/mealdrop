0347a9e84a3034104adcd3b4f5b316e1c9b2d515
Yann Braga <yannbf@gmail.com>
2025-11-04 16:41:53 +0100

    use onboarding canary

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,24 +1,17 @@
-import { StorybookConfig } from '@storybook/react-vite'
+import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: [
-    '../src/docs/Introduction.mdx',
-    '../src/docs/*.mdx',
-    '../src/**/*.mdx',
-    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  addons: [
-    '@storybook/addon-vitest',
-    '@storybook/addon-a11y',
-    // TODO: this addon has a bad instrumentation of getConfig.asyncWrapper that should be fixed
-    'storybook-addon-test-codegen',
-    '@storybook/addon-designs',
-    '@storybook/addon-docs',
+  "addons": [
+    "@storybook/addon-docs",
+    "@storybook/addon-onboarding"
   ],
-  typescript: {
-    reactDocgen: 'react-docgen',
-  },
-  staticDirs: ['../public'],
-  framework: '@storybook/react-vite',
-}
-export default config
+  "framework": {
+    "name": "@storybook/react-vite",
+    "options": {}
+  }
+};
+export default config;
