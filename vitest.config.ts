9e9a40239ef2f72de19da98d180b69b8e22f79c1
yannbf <yannbf@gmail.com>
2025-12-22 14:35:59 +0100

    add defineConfig

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,29 +1,32 @@
-import { mergeConfig, coverageConfigDefaults } from 'vitest/config'
+import { mergeConfig, defineConfig, coverageConfigDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
 
-export default mergeConfig(viteConfig, {
-  test: {
-    name: 'node',
-    environment: 'happy-dom',
-    include: ['**/*.test.ts'],
-    coverage: {
-      exclude: [
-        ...coverageConfigDefaults.exclude,
-        'storybook.setup.ts',
-        '**/*.stories.*',
-        '.storybook',
-        'src/docs',
-        'src/components/Button/utils.tsx',
-        'build',
-        'public',
-        'functions',
-        '**/conditional-logic.ts',
-        '**/RestaurantCard/progress',
-        '**/RestaurantsSection.container.tsx',
-        'src/stub',
-        'ps-setup.ts',
-        '**/serviceWorker.ts',
-      ],
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      name: 'node',
+      environment: 'happy-dom',
+      include: ['**/*.test.ts'],
+      coverage: {
+        exclude: [
+          ...coverageConfigDefaults.exclude,
+          'storybook.setup.ts',
+          '**/*.stories.*',
+          '.storybook',
+          'src/docs',
+          'src/components/Button/utils.tsx',
+          'build',
+          'public',
+          'functions',
+          '**/conditional-logic.ts',
+          '**/RestaurantCard/progress',
+          '**/RestaurantsSection.container.tsx',
+          'src/stub',
+          'ps-setup.ts',
+          '**/serviceWorker.ts',
+        ],
+      },
     },
-  },
-})
+  })
+)
