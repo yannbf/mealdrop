a7fd08d91f56fa15a4f60d8eca82dec36478709f
Julien Huang <julien.h.dev@gmail.com>
2026-06-29 14:36:20 +0200

    feat: use vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { experimental_vitePlugin } from '@storybook/builder-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), experimental_vitePlugin()],
   build: {
     outDir: 'build',
   },
