4466a6519d0f99587303a9b458de9601fbc71ca8
Julien Huang <julien.h.dev@gmail.com>
2026-07-16 15:11:30 +0200

    chore: update packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { DevTools } from '@vitejs/devtools'
+import sbPlugin from 'vite-plugin-experimental-storybook-devtools/react'
 import { experimental_vitePlugin } from '@storybook/builder-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), experimental_vitePlugin()],
+  plugins: [
+    react(),
+    DevTools(),
+    experimental_vitePlugin(),
+    sbPlugin({
+      storybookUrl: 'http://localhost:3000/__storybook/',
+    }),
+  ],
   build: {
     outDir: 'build',
   },
