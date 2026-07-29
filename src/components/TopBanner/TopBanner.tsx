b3b25cd13f9bf76d333125ec48917e507e18aec3
yannbf <yannbf@gmail.com>
2026-07-29 16:05:57 +0200

    fix DOM leak

--- src/components/TopBanner/TopBanner.tsx
+++ src/components/TopBanner/TopBanner.tsx
@@ -2,20 +2,20 @@ import styled, { css } from 'styled-components'
 
 import { Heading } from '../typography'
 
-const StyledHeading = styled(Heading)<{ inverted: boolean }>(
-  ({ inverted, theme: { color } }) => css`
-    color: ${inverted ? color.white : color.primaryText};
+const StyledHeading = styled(Heading)<{ $inverted: boolean }>(
+  ({ $inverted, theme: { color } }) => css`
+    color: ${$inverted ? color.white : color.primaryText};
   `
 )
 
-const Container = styled.div<{ src?: string }>(
-  ({ src, theme: { color } }) => css`
+const Container = styled.div<{ $src?: string }>(
+  ({ $src, theme: { color } }) => css`
     width: 100%;
     display: flex;
     position: relative;
     justify-content: center;
     align-items: center;
-    background: ${src ? `url(${src}) no-repeat 50%` : color.topBannerBackground};
+    background: ${$src ? `url(${$src}) no-repeat 50%` : color.topBannerBackground};
     background-position: center;
     background-size: cover;
     height: 240px;
@@ -29,8 +29,8 @@ type TopBannerProps = {
 }
 
 export const TopBanner = ({ photoUrl, title }: TopBannerProps) => (
-  <Container src={photoUrl}>
+  <Container $src={photoUrl}>
     {/* <StyledIconButton onClick={onBackClick} small name="arrow-left" /> */}
-    {title && <StyledHeading inverted={!!photoUrl}>{title}</StyledHeading>}
+    {title && <StyledHeading $inverted={!!photoUrl}>{title}</StyledHeading>}
   </Container>
 )
